6ea0f1a445b98924aca6031c27b3f8b7624d2e34
coderanmo <111891010+coderanmo@users.noreply.github.com>
2025-12-09 18:38:44 +0530

    Update axios base URL to production endpoint

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-    baseURL : 'http://localhost:4000/'
+    baseURL : 'https://techbridge-expencetracker-backend.onrender.com'
 })
