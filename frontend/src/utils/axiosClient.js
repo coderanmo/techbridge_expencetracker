4d7a677f26a720ee002905ee37614b86874787a2
coderanmo <111891010+coderanmo@users.noreply.github.com>
2025-12-09 18:39:29 +0530

    Fix baseURL to include trailing slash

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-    baseURL : 'https://techbridge-expencetracker-backend.onrender.com'
+    baseURL : 'https://techbridge-expencetracker-backend.onrender.com/'
 })
