8aad746de4cd100f99283fba5272ee281856b125
coderanmo <111891010+coderanmo@users.noreply.github.com>
2025-12-09 18:30:24 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,7 @@
 const mongoos = require('mongoose');
 const connectDB = async()=>{
     try {
-        await mongoos.connect('mongodb://127.0.0.1:27017/techbridge')
+        await mongoos.connect('mongodb+srv://anmolyadav639944_db_user:lB0d7lA3ZfXjL9Kc@cluster0.vjcmzfa.mongodb.net/techbridge')
             console.log("Connected!!!")
     } catch (error) {
         console.log("Not Connected!!")
